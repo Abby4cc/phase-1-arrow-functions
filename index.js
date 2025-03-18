27de2ecc62aa63713908daf811935417e5498b16
Abigail Chelanagt <abigailchelah@gmail.com>
2025-03-18 13:18:00 -0700

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//  Function expression to return a number divided by 100
+const divide = function(num) {
+    return num / 100;
+};
+
+//  Arrow function to square a number
+const square = num => num * num;
+
+//  Arrow function to add two numbers
+const add = (num1, num2) => num1 + num2;
